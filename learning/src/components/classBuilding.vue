0c074d1359df3c014dc52d526aedad8f36dd9217
Bibek Timilsina <bibektimilsina000@gmail.com>
2022-05-25 00:39:17 +0545

    commit

---
+++ learning/src/components/classBuilding.vue
@@ -0,0 +1,43 @@
+<template>
+  <h1 :class="styleApply">class binding component</h1>
+  <button v-on:click="(isGreen = !isGreen)">
+    apply style
+  </button>
+</template>
+
+<script>
+export default {
+  name: "classBulding",
+  data() {
+    return {
+      
+      isGreen: false,
+      
+    };
+  },
+  computed: {
+    styleApply: function () {
+      return {
+        green: this.isGreen,
+        err: true,
+        other:true,
+      };
+    },
+  },
+};
+</script>
+<style scoped>
+h1 {
+  color: chocolate;
+  text-align-last: center;
+}
+.green {
+  background-color: green;
+ 
+}
+.err {
+  color: red;
+}.other{
+    font-size: 50px;
+}
+</style>
---
+++ learning/src/components/mainComp.vue
@@ -0,0 +1,24 @@
+<template>
+<h1>this is main components</h1>
+<h5>{{myname}}</h5>
+<div v-html="tag"></div>
+<div v-html="tag2"></div>
+<ClassBuilding/>
+</template>
+
+<script>
+import ClassBuilding from './classBuilding.vue'
+export default {
+    name:"mainComp",
+    components:{
+        ClassBuilding
+    },
+    data(){
+        return{
+            myname:"Bibek Timilsina",
+            tag:"<h1>Bibek Timilsina</h1>",
+            tag2:"<h1>bibektimilsina000@gmai.com</h1>",
+        }
+    }
+}
+</script>
